8e0b86603d66e4049da482376e5c71932f9cf745
pydev97@gmail.com <leqthinh20530@gmail.com>
2022-04-14 09:31:16 +0700

    new file

--- js/main.js
+++ js/main.js
@@ -1,45 +1,36 @@
-const canvas = document.getElementById('canvas');
+//set key move
 const left = 'a';
 const up = 'w';
 const right = 'd';
 const down = 's';
+//----------------------------------------------------------
+//set canvas
+const canvas = document.createElement('canvas');
+canvas.setAttribute('id','canvas');
+document.getElementById('container').appendChild(canvas);
 const ctx = canvas.getContext('2d');
 let currentDirection = new Vector(-1,0)
 let gameSize = 900;
-let backgroundColor = "black";
+let backgroundColor = '#181825';
 let count = 0;
 canvas.width = canvas.height = 1000;//set width canvas - set height canvas.
 ctx.fillStyle = 'red'
 ctx.fillRect(0, 0, 1000, 100)
 ctx.font = "30px Verdana";
 ctx.fillStyle = backgroundColor
-
 ctx.fillText(`score:${count}`,5,50)
-
-
 ctx.fillStyle = backgroundColor;
-
-// ctx.fillText('Point:',0,0)
-
-
 ctx.fillRect(0, 100, 1000, gameSize)
 let snakeColor = 'white';
 let snakeUnit = 20;
-//console.log("vvlvllvlv")
 let point = new Point()
 let player1 = new Snake(point);
-
 point.spawn()
 player1.draw();
 let timeId = setInterval(function () {
-
-
         player1.move()
-
-}, 200)
-
+}, 50)
 document.onkeydown = function (e) {
-    // //console.log(e.key)
     switch (e.key) {
         case left:
             if (currentDirection.x === 1) break;
